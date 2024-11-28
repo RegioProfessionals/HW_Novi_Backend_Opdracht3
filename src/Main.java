a0d602e826c01a96016f0a22d7889cad8b855459
RegioProfessionals <richardvanbeem@live.nl>
2024-11-28 19:55:24 +0100

    Setup Git and Repositpory.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
+ // Hier mag je je code schrijven voor de hoofd-opdracht
 
-
+ // Eerste aanpassing zodat we de main kunnen pushen
     }
 }
